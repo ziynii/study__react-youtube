a3336bf3b5a527ded7fba02129a837082ce93827
Jiyeon <yeeoni_@naver.com>
2021-08-01 00:37:47 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,25 @@
-
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>hello</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA7QQjoaqclwuHy1NzQaeDSEM4ta3HITQ8',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
